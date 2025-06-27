3388251565e2e86e9d596ab8215c995b81413190
shahriyardx <mdshahriyaralam552@gmail.com>
2025-06-27 17:40:12 +0600

    initial commit

---
+++ app/src/main/java/com/shahriyardx/aspen/MainActivity.kt
@@ -0,0 +1,26 @@
+    package com.shahriyardx.aspen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.shahriyardx.aspen.ui.theme.AspenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AspenTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/shahriyardx/aspen/navigation/Screens.kt
@@ -0,0 +1,12 @@
+package com.shahriyardx.aspen.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Splash
+
+@Serializable
+object Home
+
+@Serializable
+data class Details(val id: String)
