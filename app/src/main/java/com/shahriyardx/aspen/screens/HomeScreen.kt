ee109ceffe274cd5b4d8c57b66fbdf201e52d886
shahriyardx <mdshahriyaralam552@gmail.com>
2025-06-28 03:25:51 +0600

    added header and search bar

--- app/src/main/java/com/shahriyardx/aspen/screens/HomeScreen.kt
+++ app/src/main/java/com/shahriyardx/aspen/screens/HomeScreen.kt
@@ -1,19 +1,140 @@
 package com.shahriyardx.aspen.screens
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.PinDrop
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shahriyardx.aspen.components.BottomNavigationBar
 
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Preview(showSystemUi = true)
 @Composable
 fun HomeScreen() {
     Scaffold(bottomBar = {
         BottomNavigationBar()
     }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.surfaceContainerLowest)
+                .padding(horizontal = 24.dp)
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            HomeHeader()
+            SearchBar()
+        }
+    }
+}
+
+val dropdownMenuItems = listOf("Dhaka", "Rangpur", "Sylhet")
+
+@Composable
+fun HomeHeader() {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column {
+            Text(text = "Explore", style = MaterialTheme.typography.titleMedium)
+            Text(
+                text = "Aspen",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.Medium
+            )
+        }
+
+        Box() {
+            Row(horizontalArrangement = Arrangement.spacedBy(5.dp), modifier = Modifier.clickable {
+                expanded = true
+            }) {
+                Icon(
+                    imageVector = Icons.Default.PinDrop,
+                    contentDescription = "Pin Drop",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+                Text(text = dropdownMenuItems[selectedIndex])
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowDown,
+                    contentDescription = "Pin",
+                    tint = MaterialTheme.colorScheme.primary
+                )
+
+            }
+            DropdownMenu(expanded = expanded, onDismissRequest = {
+                expanded = false
+            }) {
+                dropdownMenuItems.forEachIndexed { index, title ->
+                    DropdownMenuItem(
+                        text = { Text(title) },
+                        onClick = {
+                            selectedIndex = index
+                            expanded = false
+                        }
+                    )
+                }
+            }
+        }
 
     }
+}
+
+@Preview
+@Composable
+fun SearchBar() {
+    var text by remember { mutableStateOf("") }
+
+    OutlinedTextField(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = { Text(text = "Search") },
+        modifier = Modifier.fillMaxWidth(),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Search,
+                contentDescription = "Search",
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        },
+        maxLines = 1,
+        shape = CircleShape,
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surfaceContainer,
+            focusedContainerColor = MaterialTheme.colorScheme.surfaceContainerHigh,
+            unfocusedIndicatorColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+        )
+    )
 }
