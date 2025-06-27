89867638e9c616c3272047bafd58c0868947510b
shahriyardx <mdshahriyaralam552@gmail.com>
2025-06-28 03:53:06 +0600

    Scroll indicator

--- app/src/main/java/com/shahriyardx/aspen/screens/HomeScreen.kt
+++ app/src/main/java/com/shahriyardx/aspen/screens/HomeScreen.kt
@@ -1,18 +1,26 @@
 package com.shahriyardx.aspen.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.PinDrop
 import androidx.compose.material.icons.outlined.Search
@@ -32,6 +40,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -101,13 +110,10 @@ fun HomeHeader() {
                 expanded = false
             }) {
                 dropdownMenuItems.forEachIndexed { index, title ->
-                    DropdownMenuItem(
-                        text = { Text(title) },
-                        onClick = {
-                            selectedIndex = index
-                            expanded = false
-                        }
-                    )
+                    DropdownMenuItem(text = { Text(title) }, onClick = {
+                        selectedIndex = index
+                        expanded = false
+                    })
                 }
             }
         }
@@ -145,28 +151,51 @@ fun SearchBar() {
 @Preview
 @Composable
 fun Tabs() {
+    val scrollState = rememberScrollState()
     var selectedIndex by remember { mutableIntStateOf(0) }
     val items = listOf(
         "Location", "Hotels", "Food", "Adventure", "Attractions", "Activities"
     )
 
-    Row(
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .horizontalScroll(rememberScrollState())
+            .height(IntrinsicSize.Min)
     ) {
-        items.forEachIndexed { index, title ->
-            val selected = selectedIndex == index
-            Button(
-                onClick = {
-                    selectedIndex = index
-                }, colors = ButtonDefaults.buttonColors(
-                    containerColor = if (selected) MaterialTheme.colorScheme.surface else Color.Transparent,
-                    contentColor = if (selected) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onBackground,
-                )
-            ) {
-                Text(title, fontWeight = if (selected) FontWeight.Bold else FontWeight.Normal)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(scrollState)
+        ) {
+            items.forEachIndexed { index, title ->
+                val selected = selectedIndex == index
+                Button(
+                    onClick = {
+                        selectedIndex = index
+                    }, colors = ButtonDefaults.buttonColors(
+                        containerColor = if (selected) MaterialTheme.colorScheme.surface else Color.Transparent,
+                        contentColor = if (selected) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.onBackground,
+                    )
+                ) {
+                    Text(title, fontWeight = if (selected) FontWeight.Bold else FontWeight.Normal)
+                }
             }
         }
+
+        AnimatedVisibility(
+            scrollState.value == 0, enter = fadeIn(), exit = fadeOut(),
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(20.dp)
+                .align(Alignment.CenterEnd),
+        ) {
+            Icon(
+
+                imageVector = Icons.AutoMirrored.Default.KeyboardArrowRight,
+                contentDescription = "Right"
+            )
+        }
     }
+
+
 }
